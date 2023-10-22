b68bd3fb1941d5576302af0050cc4ecd16589232
nawawa <nawaryoga@gmail.com>
2023-10-22 13:41:34 +0900

    feat: micro-cms クライアント

---
+++ lib/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from 'microcms-js-sdk';
+
+if (!process.env.API_SERVICE_DOMAIN) {
+  throw new Error("microCMSのサービスドメインが設定されていません。")
+}
+
+if (!process.env.API_KEY) {
+  throw new Error("microCMSのAPIキーが設定されていません。")
+}
+
+export const client = createClient({
+  serviceDomain: process.env.API_SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY,
+});
