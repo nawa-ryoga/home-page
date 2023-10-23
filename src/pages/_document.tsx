3dd0f2cb08ff92f694aa119555ec55367c9f5e8c
nawawa <nawaryoga@gmail.com>
2023-10-23 12:46:25 +0900

    fix: 廃止・vanilla-extract

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { globalStyle } from '@/theme/theme.css';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className={globalStyle}>
+      <body>
         <Main />
         <NextScript />
       </body>
