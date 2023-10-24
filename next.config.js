b3874435e30bdc244f9b4d96de100cb2158b2267
nawawa <nawaryoga@gmail.com>
2023-10-24 14:37:18 +0900

    feat: 設定・microCMS の画像読み込み

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.microcms-assets.io",
+      },
+    ],
+  },
 };
 
 module.exports = withKumaUI(withVanillaExtract(nextConfig));
