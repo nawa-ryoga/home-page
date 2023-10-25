d7320c2d1657b487c36b1cda9a991506f2d1843f
nawawa <nawaryoga@gmail.com>
2023-10-25 10:41:58 +0900

    ref: ファイル移動

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,5 +1,5 @@
 import { globalStyle } from "@vanilla-extract/css";
-import { DEFAULT_BACKGROUND_COLOR } from "../kuma.config";
+import { DEFAULT_BACKGROUND_COLOR } from "../../kuma.config";
 
 /**
  * 現在(2023年10月時点)の Kuma UI では、グローバルなCSSを指定することができない
