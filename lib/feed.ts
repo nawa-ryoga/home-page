80266c2174b2ce6b1a3dd4ed64d7daac7bcbcb48
nawawa <nawaryoga@gmail.com>
2023-10-26 12:58:29 +0900

    fix: もとに戻す
    Vercelで実験するため

--- lib/feed.ts
+++ lib/feed.ts
@@ -1,33 +1,28 @@
-import { Feed } from "feed";
-import fs from "fs-extra";
-import dayjs from "dayjs"
+import RSS from "rss";
 import { getBlogList } from "./client";
-import { DOMAIN, SITE_TITLE, OG_DESCRIPTION } from "../kuma.config";
+import { DOMAIN as D, SITE_TITLE, OG_DESCRIPTION } from "../kuma.config";
 
 export async function generateFeedXml() {
+  const DOMAIN = process.env.NODE_ENV === "production" ? D : "localhost";
+  const PROTOCOL = process.env.NODE_ENV === "production" ? "https" : "http";
 
-  const baseUrl = `https://${DOMAIN}`;
-
-  const feed = new Feed({
+  const feed = new RSS({
     title: SITE_TITLE,
     description: OG_DESCRIPTION,
-    link: baseUrl,
-    feed: `${baseUrl}/feed`,
+    site_url: `${PROTOCOL}://${DOMAIN}`,
+    feed_url: `${PROTOCOL}://${DOMAIN}/feed`,
     language: "ja",
-    id: baseUrl,
-    copyright: `©${dayjs().toDate().getFullYear()} naary.me`,
   });
 
   const { contents } = await getBlogList();
   contents?.forEach((post) => {
-    feed.addItem({
+    feed.item({
       title: post.title,
       description: post.summary,
       date: new Date(post.createdAt),
-      link: `${DOMAIN}/blogs/${post.id}`,
+      url: `${DOMAIN}/blogs/${post.id}`,
     });
   });
 
-  fs.mkdirSync("./public/rss", { recursive: true });
-  fs.writeFileSync("./public/rss/feed.xml", feed.rss2());
+  return feed.xml();
 }
