6d3eba267ca3a95cf884029d2836e7c3fb5f63ce
nawawa <nawaryoga@gmail.com>
2023-11-05 15:15:06 +0900

    feat: 記事0件への対応

--- src/components/Routes/Blogs/index.tsx
+++ src/components/Routes/Blogs/index.tsx
@@ -44,84 +44,103 @@ export default function Page({ blogs }: Props) {
           marginX={"auto"}
           gap={48}
         >
-          {blogs.map((blog) => (
+          {!blogs.length && (
             <VStack
-              key={blog.id}
               justifyContent={"center"}
               alignItems={"center"}
-              position={"relative"}
             >
-              <Flex
-                as={"span"}
-                maxWidth={["180px", "280px"]}
+              <Text
+                as={"h2"}
+                fontSize={[theme.fontSizes["fontSizes.md"], theme.fontSizes["fontSizes.xl"]]}
+                letterSpacing={"0.05rem"}
+                textAlign={"center"}
+              >
+                記事がありません
+              </Text>
+            </VStack>
+          )}
+          {blogs.length >= 1 &&
+            blogs.map((blog) => (
+              <VStack
+                key={blog.id}
                 justifyContent={"center"}
                 alignItems={"center"}
+                position={"relative"}
               >
-                <Link
-                  className={defaultInnerLinkStyle}
-                  href={`/blogs/${blog.id}`}
+                <Flex
+                  as={"span"}
+                  maxWidth={["180px", "280px"]}
+                  justifyContent={"center"}
+                  alignItems={"center"}
                 >
-                  <Image
-                    src={`${blog.eyecatch.url}`}
-                    srcSet={`${blog.eyecatch.url}?w=180 180w, ${blog.eyecatch.url}?w=240 240w`}
-                    maxWidth={"100%"}
-                    height={"auto"}
-                    alt={""}
-                    aspectRatio={"1 / 1"}
-                  />
-                </Link>
-              </Flex>
-              <Box
-                as={"span"}
-                marginX={["-16px", "-32px"]}
-                marginTop={["-42px", "-78px"]}
-                paddingX={["16px", 0]}
-              >
-                <Link
-                  className={defaultInnerLinkStyle}
-                  href={`/blogs/${blog.id}`}
+                  <Link
+                    className={defaultInnerLinkStyle}
+                    href={`/blogs/${blog.id}`}
+                  >
+                    <Image
+                      src={`${blog.eyecatch.url}`}
+                      srcSet={`${blog.eyecatch.url}?w=180 180w, ${blog.eyecatch.url}?w=240 240w`}
+                      maxWidth={"100%"}
+                      height={"auto"}
+                      alt={""}
+                      aspectRatio={"1 / 1"}
+                    />
+                  </Link>
+                </Flex>
+                <Box
+                  as={"span"}
+                  marginX={["-16px", "-32px"]}
+                  marginTop={["-42px", "-78px"]}
+                  paddingX={["16px", 0]}
                 >
-                  <Text
-                    as={"h2"}
-                    fontSize={[theme.fontSizes["fontSizes.md"], theme.fontSizes["fontSizes.xl"]]}
-                    letterSpacing={"0.05rem"}
-                    textAlign={"center"}
-                    marginBottom={["12px", "16px"]}
+                  <Link
+                    className={defaultInnerLinkStyle}
+                    href={`/blogs/${blog.id}`}
                   >
-                    {blog.title}
-                  </Text>
-                </Link>
-
-                {blog.publishedAt && (
-                  <VStack
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    gap={[4, 8]}
-                  >
-                    <Box
-                      as={"span"}
-                      fontSize={[
-                        theme.fontSizes["fontSizes.sm"],
-                        theme.fontSizes["fontSizes.base"],
-                      ]}
-                      color={"colors.font.darken.1"}
-                      letterSpacing={"0.1rem"}
-                    >
-                      {publishedDate(blog.publishedAt)}
-                    </Box>
-                    <Box
-                      as={"span"}
-                      fontSize={[theme.fontSizes["fontSizes.xs"], theme.fontSizes["fontSizes.sm"]]}
-                      color={"colors.font.darken.1"}
+                    <Text
+                      as={"h2"}
+                      fontSize={[theme.fontSizes["fontSizes.md"], theme.fontSizes["fontSizes.xl"]]}
                       letterSpacing={"0.05rem"}
+                      textAlign={"center"}
+                      marginBottom={["12px", "16px"]}
                     >
-                      {publishedYear(blog.publishedAt)}
-                    </Box>
-                  </VStack>
-                )}
-              </Box>
-            </VStack>
-          ))}
+                      {blog.title}
+                    </Text>
+                  </Link>
+
+                  {blog.publishedAt && (
+                    <VStack
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                      gap={[4, 8]}
+                    >
+                      <Box
+                        as={"span"}
+                        fontSize={[
+                          theme.fontSizes["fontSizes.sm"],
+                          theme.fontSizes["fontSizes.base"],
+                        ]}
+                        color={"colors.font.darken.1"}
+                        letterSpacing={"0.1rem"}
+                      >
+                        {publishedDate(blog.publishedAt)}
+                      </Box>
+                      <Box
+                        as={"span"}
+                        fontSize={[
+                          theme.fontSizes["fontSizes.xs"],
+                          theme.fontSizes["fontSizes.sm"],
+                        ]}
+                        color={"colors.font.darken.1"}
+                        letterSpacing={"0.05rem"}
+                      >
+                        {publishedYear(blog.publishedAt)}
+                      </Box>
+                    </VStack>
+                  )}
+                </Box>
+              </VStack>
+            ))}
         </VStack>
       </Main>
 
