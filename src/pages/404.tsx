67d51628bf7505934ac5baacc79b4aca3ed0b539
nawawa <nawaryoga@gmail.com>
2023-11-08 12:40:42 +0900

    ref: 共通化

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,10 @@
 import Header from "@/components/Layouts/Header";
 import Footer from "@/components/Layouts/Footer";
 import Main from "@/components/Layouts/Main";
+import ErrorPageContainer from "@/components/Layouts/Error/Parts/Container";
+import ErrorPageHeader from "@/components/Layouts/Error/Parts/Header";
 import Image from "next/image";
-import { VStack, Text } from "@kuma-ui/core";
+import { Text } from "@kuma-ui/core";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -19,37 +21,22 @@ export default function Error404() {
       <Header title="404" />
 
       <Main>
-        <VStack
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <VStack
-            justifyContent={"center"}
-            alignItems={"center"}
-            gap={[24, 48]}
-          >
-            <VStack
-              as="span"
-              justifyItems={"center"}
-              alignItems={"center"}
-              gap={8}
-              paddingTop={[16, 32]}
-            >
-              <Text letterSpacing={"0.1rem"}>{url}</Text>
-              <Text color={"colors.font.darken.1"}>is Not Found.</Text>
-            </VStack>
-            <Image
-              src={
-                "https://images.microcms-assets.io/assets/c987b67b4da34a3d8860df3dc1a06811/c4f45147f3754fb0b886d21aef8364f7/500.png"
-              }
-              alt={""}
-              width={360}
-              height={360}
-              quality={100}
-              style={{ borderRadius: "8px", height: "auto", maxWidth: "100%" }}
-            />
-          </VStack>
-        </VStack>
+        <ErrorPageContainer>
+          <ErrorPageHeader>
+            <Text letterSpacing={"0.1rem"}>{url}</Text>
+            <Text color={"colors.font.darken.1"}>is Not Found.</Text>
+          </ErrorPageHeader>
+          <Image
+            src={
+              "https://images.microcms-assets.io/assets/c987b67b4da34a3d8860df3dc1a06811/c4f45147f3754fb0b886d21aef8364f7/500.png"
+            }
+            alt={""}
+            width={360}
+            height={360}
+            quality={100}
+            style={{ borderRadius: "8px", height: "auto", maxWidth: "100%" }}
+          />
+        </ErrorPageContainer>
       </Main>
 
       <Footer />
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,8 +1,10 @@
 import Header from "@/components/Layouts/Header";
 import Footer from "@/components/Layouts/Footer";
 import Main from "@/components/Layouts/Main";
+import ErrorPageContainer from "@/components/Layouts/Error/Parts/Container";
+import ErrorPageHeader from "@/components/Layouts/Error/Parts/Header";
 import Image from "next/image";
-import { VStack, Text } from "@kuma-ui/core";
+import { Text } from "@kuma-ui/core";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -19,37 +21,22 @@ export default function Error500() {
       <Header title="500" />
 
       <Main>
-        <VStack
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <VStack
-            justifyContent={"center"}
-            alignItems={"center"}
-            gap={[24, 48]}
-          >
-            <VStack
-              as="span"
-              justifyItems={"center"}
-              alignItems={"center"}
-              gap={8}
-              paddingTop={[16, 32]}
-            >
-              <Text color={"colors.font.darken.1"}>Server Error on</Text>
-              <Text letterSpacing={"0.1rem"}>{url}</Text>
-            </VStack>
-            <Image
-              src={
-                "https://images.microcms-assets.io/assets/c987b67b4da34a3d8860df3dc1a06811/e0ab32414bb94d8e9d56121c9a2dc0b6/404.png"
-              }
-              alt={""}
-              width={600}
-              height={600}
-              quality={100}
-              style={{ borderRadius: "8px", height: "auto", maxWidth: "100%" }}
-            />
-          </VStack>
-        </VStack>
+        <ErrorPageContainer>
+          <ErrorPageHeader>
+            <Text color={"colors.font.darken.1"}>Server Error on</Text>
+            <Text letterSpacing={"0.1rem"}>{url}</Text>
+          </ErrorPageHeader>
+          <Image
+            src={
+              "https://images.microcms-assets.io/assets/c987b67b4da34a3d8860df3dc1a06811/e0ab32414bb94d8e9d56121c9a2dc0b6/404.png"
+            }
+            alt={""}
+            width={600}
+            height={600}
+            quality={100}
+            style={{ borderRadius: "8px", height: "auto", maxWidth: "100%" }}
+          />
+        </ErrorPageContainer>
       </Main>
 
       <Footer />
