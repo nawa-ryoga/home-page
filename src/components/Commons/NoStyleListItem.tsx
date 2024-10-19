7b0e0b598da9195755ce1a2012af8933460413c9
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-10-19 00:34:24 +0000

    全てのソースを削除

--- src/components/Commons/NoStyleListItem.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@kuma-ui/core";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function NonStyleListItem({ children }: Props) {
-  return <Box as={"li"}>{children}</Box>;
-}
--- src/styles/font.ts
+++
@@ -1,6 +0,0 @@
-import { Noto_Sans_JP } from "next/font/google";
-
-export const NOTO = Noto_Sans_JP({
-  weight: ["400", "700"],
-  preload: false,
-});
