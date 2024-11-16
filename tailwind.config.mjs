8bee8fc332192f001e1bfce207e6589b7b0a4bf5
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-16 05:11:28 +0000

    feat: カラーコードの定数読み込みテスト

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+// import { THEME_COLOR } from "./src/styles/consts/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		colors: {
+			background: {
+				default: THEME_COLOR,
+			},
+		},
 		extend: {},
 	},
 	plugins: [],
-}
+};
