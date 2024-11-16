fe1293c7b95aa10ece6c6f5b8824265f2577e3c7
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-16 05:44:23 +0000

    fix: デフォルトの背景色を追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,10 @@
-// import { THEME_COLOR } from "./src/styles/consts/colors";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
 	theme: {
 		colors: {
 			background: {
-				default: THEME_COLOR,
+				default: "#232136",
 			},
 		},
 		extend: {},
