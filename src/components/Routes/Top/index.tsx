918c72c157ed273bf355b6a5fc4801176865f7c2
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-16 08:57:42 +0000

    fix: 旧各ページ用コンポーネントを廃止

--- src/components/Routes/Top/index.tsx
+++
@@ -1,9 +0,0 @@
-import Meta from "../../Layouts/Meta";
-
-export default function Top() {
-	return (
-		<Meta title={"NAARY.ME"} content={"Personal website of naary"}>
-			<h1>Astroです</h1>
-		</Meta>
-	);
-}
