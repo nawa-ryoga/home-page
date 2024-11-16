ee20c5bf46a83b28a6a70c2a034b8c775cfa3b6c
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-16 06:56:55 +0000

    feat: スタイルはほぼ全てTailwindで管理する

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,14 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
 	theme: {
 		colors: {
-
+			background: {
+				default: "#232136",
+				lighten: { 1: "#2A283C" },
+				darken: { 1: "#151424" },
+			},
+			text: {
+				default: "white",
+			},
 		},
 		extend: {},
 	},
