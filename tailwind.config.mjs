5f95bc0e8c36b084f0c8405dbfca0c744dab061a
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-23 06:47:22 +0000

    feat: 背景とテキストの色にバリエーションを追加

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,19 @@ export default {
 		colors: {
 			background: {
 				default: "#232136",
-				lighten: { 1: "#2A283C" },
+				lighten: {
+					1: "#2A283C",
+					2: "#403E50",
+					3: "#767581",
+				},
 				darken: { 1: "#151424" },
 			},
 			text: {
 				default: "white",
+				darken: {
+					1: "#c9e0e6",
+					2: "#9E9DA6",
+				},
 			},
 		},
 		extend: {
