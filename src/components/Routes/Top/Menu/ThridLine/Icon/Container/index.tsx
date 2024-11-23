2c74533f4a83491509c1ad715edf466ca270ab51
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-23 06:55:11 +0000

    fix: アイコンの規定サイズ

--- src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
+++ src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
@@ -10,7 +10,7 @@ export default function IconContainer({ imageUrl, href }: Props) {
 				href={href}
 				target="_blank"
 				rel="noopener nofollow noreferrer"
-				className="w-[56px] sm:w-[84px] aspect-square"
+				className="w-[48px] sm:w-[84px] aspect-square"
 			>
 				<img className="w-full h-full invert" src={imageUrl} alt="" />
 			</a>
