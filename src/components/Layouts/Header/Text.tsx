78dbaa01ae2381396509b4b0460b39fc3dece152
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-23 06:51:26 +0000

    ref: 使ってない色指定の削除

--- src/components/Layouts/Header/Text.tsx
+++ src/components/Layouts/Header/Text.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function HeaderText({ content }: Props) {
 	return (
 		<div className="pt-16 pb-2">
-			<p className="text-sm font-bold text-center text-gray-700">{content}</p>
+			<p className="text-sm font-bold text-center">{content}</p>
 		</div>
 	);
 }
