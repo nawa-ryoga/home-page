071b005845a207387b4ba620cbb79788cb4a3a2c
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-11-24 04:07:37 +0000

    fix: リンクが効いてなかったのを修正

--- src/components/Routes/Top/Menu/FirstLine/Item/index.tsx
+++ src/components/Routes/Top/Menu/FirstLine/Item/index.tsx
@@ -2,25 +2,26 @@ import type { MicroCMSImage } from "microcms-js-sdk";
 
 type Props = {
 	image: MicroCMSImage;
-	href: string
+	href: string;
 	title: string;
 };
 
 export default function FirstLineItem({ image, href, title }: Props) {
 	return (
 		<li className="relative">
-			<a href={`/${href}`}>
-				<img
-					className="object-cover max-h-[8rem] w-full rounded-xl z-10"
-					src={`${image.url}?w=360`}
-					alt=""
-				/>
-			</a>
-			<div className="absolute z-20 w-full h-full inset-0 bg-background-default bg-opacity-50 rounded-xl">
-				<div className="flex items-center h-full px-4 font-bold tracking-[0.3rem]">
+			<img
+				className="object-cover max-h-[8rem] w-full rounded-xl z-10"
+				src={`${image.url}?w=360`}
+				alt=""
+			/>
+			<a
+				href={`/${href}`}
+				className="absolute z-20 w-full h-full inset-0 bg-background-default bg-opacity-50 rounded-xl"
+			>
+				<span className="flex items-center h-full px-4 font-bold tracking-[0.3rem]">
 					{title}
-				</div>
-			</div>
+				</span>
+			</a>
 		</li>
 	);
 }
