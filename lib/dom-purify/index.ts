373da11d0b929c9a4ccefdeeb6b22dbeedfb9549
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-12-08 04:03:40 +0000

    feat: DOMPurifyとJSDOM

---
+++ lib/dom-purify/index.ts
@@ -0,0 +1,16 @@
+import DOMPurify from "dompurify";
+import type { DOMPurify as DOMPurifyType } from "dompurify";
+import { JSDOM } from "jsdom";
+
+export const purify = () => {
+	const window = new JSDOM("").window;
+	const purify = DOMPurify(window);
+
+	return purify;
+};
+
+export const sanitize = (evilHTML: string, purify: DOMPurifyType) => {
+	return purify.sanitize(evilHTML, {
+		USE_PROFILES: { html: true },
+	});
+};
