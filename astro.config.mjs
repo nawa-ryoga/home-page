0714404b2983994decafdff8cf461cd4e597f717
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-12-15 01:26:31 +0000

    feat: Cloudflareのアダプター＆オンデマンドレンダリングの実験

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
+import cloudflare from "@astrojs/cloudflare";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: cloudflare(),
 	integrations: [tailwind(), react()],
 	vite: {
 		plugins: [vanillaExtractPlugin()],
