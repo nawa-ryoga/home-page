861e3c0ae3bd19319365164dacddbdd87dfd645d
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-12-28 04:50:22 +0000

    feat: cssファイルでもTailwindのトークンを使用する

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+	content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,css}"],
 	theme: {
 		colors: {
 			background: {
