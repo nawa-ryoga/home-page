9a1e1053656a2f7d58650b3eaf0f240548fdcdbd
naary <66899072+nawa-ryoga@users.noreply.github.com>
2024-12-28 12:51:41 +0000

    test: vercelで試す

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,15 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
-import cloudflare from "@astrojs/cloudflare";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
-	vite: {
-		plugins: [vanillaExtractPlugin()],
+  	integrations: [tailwind(), react()],
+
+  	vite: {
+      plugins: [vanillaExtractPlugin()],
 	},
-	output: "server",
-	adapter: cloudflare(),
+  	output: "server",
+  	adapter: vercel(),
 });
