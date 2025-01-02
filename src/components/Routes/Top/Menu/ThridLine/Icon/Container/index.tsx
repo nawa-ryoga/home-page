4ca8cc749ee7201da0a06c77af646f6e3086d6c1
naary <66899072+nawa-ryoga@users.noreply.github.com>
2025-01-02 03:44:09 +0000

    fix: トップ画面のデザインを変更
    Instagramのリンクを廃止する関係で

--- src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
+++ src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
@@ -5,14 +5,14 @@ type Props = {
 
 export default function IconContainer({ imageUrl, href }: Props) {
 	return (
-		<li className="aspect-square flex items-center justify-center rounded-xl">
+		<li className="aspect-square flex items-center justify-center rounded-xl bg-background-lighten-1">
 			<a
 				href={href}
 				target="_blank"
 				rel="noopener nofollow noreferrer"
-				className="w-[48px] sm:w-[84px] aspect-square"
+				className="w-[48px] sm:w-[84px] aspect-square bg-text-darken-1 rounded-lg flex items-center justify-center box-border"
 			>
-				<img className="w-full h-full invert" src={imageUrl} alt="" />
+				<img className="w-[70%] h-[70%] object-contain" src={imageUrl} alt="" />
 			</a>
 		</li>
 	);
