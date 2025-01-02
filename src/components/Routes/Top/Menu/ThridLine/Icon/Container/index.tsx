366456ad1047672423af1f422fc18a071ebab530
naary <66899072+nawa-ryoga@users.noreply.github.com>
2025-01-02 05:22:30 +0000

    feat: snsのリンクへaltテキストを追加

--- src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
+++ src/components/Routes/Top/Menu/ThridLine/Icon/Container/index.tsx
@@ -1,18 +1,19 @@
 type Props = {
 	href: string;
 	imageUrl: string;
+	alt: string;
 };
 
-export default function IconContainer({ imageUrl, href }: Props) {
+export default function IconContainer({ imageUrl, href, alt }: Props) {
 	return (
 		<li className="aspect-square flex items-center justify-center rounded-xl bg-background-lighten-1">
 			<a
 				href={href}
 				target="_blank"
 				rel="noopener nofollow noreferrer"
-				className="w-[48px] sm:w-[84px] aspect-square bg-text-darken-1 rounded-lg flex items-center justify-center box-border"
+				className="w-[48px] sm:w-[84px] aspect-square"
 			>
-				<img className="w-[70%] h-[70%] object-contain" src={imageUrl} alt="" />
+				<img className="" src={imageUrl} alt={alt} />
 			</a>
 		</li>
 	);
