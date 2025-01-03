e1e7aa9988201068109341e60fed195e0939ff08
naary <66899072+nawa-ryoga@users.noreply.github.com>
2025-01-03 01:41:53 +0000

    feat: RSSフィードの配信用xmlを追加

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,19 @@
+import rss from "@astrojs/rss";
+import { getBlogList } from "../../lib/cms-client";
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext) {
+	const blogs = await getBlogList();
+
+	return rss({
+		title: "NAARY.ME",
+		description: "Personal website of naary",
+		site: context.site?.origin ?? "https://naary.me",
+		items: blogs.map((blog) => ({
+			title: blog.title,
+			description: blog.summary,
+			link: `/blog/${blog.id}`,
+			pubDate: new Date(blog.createdAt),
+		})),
+	});
+}
