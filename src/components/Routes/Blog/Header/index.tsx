c3807bd6ba7fa5fe31483bf7ddeea6a578d3c809
naary <66899072+nawa-ryoga@users.noreply.github.com>
2025-01-03 13:00:25 +0000

    fix: スマホ向けスタイルの調整

--- src/components/Routes/Blog/Header/index.tsx
+++ src/components/Routes/Blog/Header/index.tsx
@@ -15,7 +15,7 @@ export default function HeaderContainer({
 	publishedAtDate,
 }: Props) {
 	return (
-		<header className="py-12 pb-[40px] md:pb-12 gap-12">
+		<header className="py-12 pb-[40px] md:pb-12">
 			<div className="flex flex-col justify-center items-center relative">
 				<div className="max-w-[320px] md:max-w-[480px] flex justify-center items-center">
 					<img
@@ -28,10 +28,10 @@ export default function HeaderContainer({
 						fetchPriority="high"
 					/>
 				</div>
-				<div className="md:mx-[-32px] mt-[-42px] md:mt-[-78px] px-[16px] md:px-0">
-					<div className="flex flex-col justify-center gap-8">
-						<h1 className="text-4xl font-bold text-center">{title}</h1>
-						<div className="text-xl text-right tracking-[0.05rem] text-text-darken-1">
+				<div className="md:mx-[-32px] mt-[-56px] md:mt-[-78px] px-[16px] md:px-0">
+					<div className="flex flex-col justify-center gap-4 sm:gap-8">
+						<h1 className="text-2xl sm:text-4xl font-bold text-center">{title}</h1>
+						<div className="text-md sm:text-xl text-right tracking-[0.05rem] text-text-darken-1">
 							{summery}
 						</div>
 						<div className="flex flex-col justify-center items-center gap-2 md:gap-[8px]">
