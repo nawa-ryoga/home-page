28aeeafe133973d123e4d54fdcc5e0e068e5c66d
naary <66899072+nawa-ryoga@users.noreply.github.com>
2025-01-03 06:39:26 +0000

    fix: 記事詳細のスタイルを調整

---
+++ src/components/Routes/Blog/Content/index.tsx
@@ -0,0 +1,13 @@
+type Props = {
+	children: React.ReactNode;
+};
+
+export default function ContentContainer({ children }: Props) {
+	return (
+		<div className="flex flex-col items-center w-full">
+			<div className="border-t-[1px] border-text-darken-2 max-w-[620px] pt-20 sm:pt-40">
+				{children}
+			</div>
+		</div>
+	);
+}
